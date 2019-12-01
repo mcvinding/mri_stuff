d74ded8a723c81bcd388ce810ddb7e7da940a20f
mcvinding <mikkel.vinding@gmail.com>
2019-12-01 23:18:13 +0100

    compared dipole localization error

--- warpig/compare_dipoles.m
+++ warpig/compare_dipoles.m
@@ -7,17 +7,32 @@ ft_defaults
 data_path = '/home/mikkel/mri_scripts/warpig/data/0177';
 
 %% Load data
-
+load(fullfile(data_path, 'dip_mag_early.mat'))
+load(fullfile(data_path, 'dip_mag_late.mat'))
+load(fullfile(data_path, 'dip_grad_early.mat'))
+load(fullfile(data_path, 'dip_grad_late.mat'))
 
 %% Compare dip: mags early component
-
+norm(dip_mag_early_orig.dip.pos-dip_mag_early_lin.dip.pos)
+norm(dip_mag_early_orig.dip.pos-dip_mag_early_spm12.dip.pos)
+norm(dip_mag_early_spm12.dip.pos-dip_mag_early_lin.dip.pos)
 
 %% Compare dip: mags late component
-
+norm(dip_mag_late_orig.dip.pos(1,:)-dip_mag_late_lin.dip.pos(2,:))
+norm(dip_mag_late_orig.dip.pos(1,:)-dip_mag_late_spm12.dip.pos(1,:))
+norm(dip_mag_late_spm12.dip.pos(1,:)-dip_mag_late_lin.dip.pos(2,:))
 
 %% Compare dip: grads early component
-
+norm(dip_grad_early_orig.dip.pos-dip_grad_early_lin.dip.pos)
+norm(dip_grad_early_orig.dip.pos-dip_grad_early_spm12.dip.pos)
+norm(dip_grad_early_spm12.dip.pos-dip_grad_early_lin.dip.pos)
 
 %% Compare dip: grads late component
+norm(dip_grad_late_orig.dip.pos(1,:)-dip_grad_late_lin.dip.pos(2,:))
+norm(dip_grad_late_orig.dip.pos(1,:)-dip_grad_late_spm12.dip.pos(1,:))
+norm(dip_grad_late_spm12.dip.pos(1,:)-dip_grad_late_lin.dip.pos(2,:))
 
 
+
+
+%END
--- warpig/dipole_analysis.m
+++ warpig/dipole_analysis.m
@@ -16,6 +16,8 @@ load(fullfile(data_path, 'headmodel_spm12.mat'));
 load(fullfile(data_path, 'sourcemodel_lin.mat'));
 load(fullfile(data_path, 'sourcemodel_orig.mat'));
 load(fullfile(data_path, 'sourcemodel_spm12.mat'));
+load(fullfile(data_path, 'grad'));    % !!!
+
 disp('Done');
 
 data = timelockeds{4};   % Only indexfinger stim.
@@ -57,7 +59,7 @@ cfg.dipfit.metric       = 'rv';         %% the metric to minimize (the relative 
 cfg.model               = 'regional';   %% we assume that the dipole has a fixed position during the time points in the latency range
 cfg.senstype            = 'MEG';        %% sensor type
 cfg.channel             = 'megmag';     %% which channels to use
-cfg.grad                = grad_raw;
+cfg.grad                = grad;
 cfg.nonlinear           = 'yes';        %% do a non-linear search
 
 % FIRST COMPONENT
@@ -66,25 +68,24 @@ cfg.numdipoles          = 1; %% we only expect contralateral activity
 cfg.symmetry            = []; %% empty for single dipole fit
 
 % linear
-cfg.sourcemodel         = sourcemodel_lin;
-
-cfg.sourcemodel.pos     = sourcemodel_lin.pos;
-cfg.sourcemodel.inside  = sourcemodel_lin.inside;
-cfg.sourcemodel.dim     = sourcemodel_lin.dim;
+% cfg.sourcemodel         = sourcemodel_lin;
+% cfg.sourcemodel.pos     = sourcemodel_lin.pos;
+% cfg.sourcemodel.inside  = sourcemodel_lin.inside;
+% cfg.sourcemodel.dim     = sourcemodel_lin.dim;
 cfg.headmodel           = headmodel_lin;
 dip_mag_early_lin = ft_dipolefitting(cfg, meg_data);
 
 % orig
-cfg.sourcemodel.pos     = sourcemodel_orig.pos;
-cfg.sourcemodel.inside  = sourcemodel_orig.inside;
-cfg.sourcemodel.dim     = sourcemodel_orig.dim;
+% cfg.sourcemodel.pos     = sourcemodel_orig.pos;
+% cfg.sourcemodel.inside  = sourcemodel_orig.inside;
+% cfg.sourcemodel.dim     = sourcemodel_orig.dim;
 cfg.headmodel           = headmodel_orig; %% supply the headmodel
 dip_mag_early_orig = ft_dipolefitting(cfg, meg_data);
 
 % spm12
-cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
-cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
-cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
+% cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
+% cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
+% cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
 cfg.headmodel           = headmodel_spm12; %% supply the headmodel
 dip_mag_early_spm12 = ft_dipolefitting(cfg, meg_data);
 
@@ -94,35 +95,36 @@ cfg.numdipoles          = 2;    %% we expect bilateral activity
 cfg.symmetry            = 'x';
 
 % linear
-cfg.sourcemodel.pos     = sourcemodel_lin.pos;
-cfg.sourcemodel.inside  = sourcemodel_lin.inside;
-cfg.sourcemodel.dim     = sourcemodel_lin.dim;
+% cfg.sourcemodel.pos     = sourcemodel_lin.pos;
+% cfg.sourcemodel.inside  = sourcemodel_lin.inside;
+% cfg.sourcemodel.dim     = sourcemodel_lin.dim;
 cfg.headmodel           = headmodel_lin; %% supply the headmodel
 dip_mag_late_lin = ft_dipolefitting(cfg, meg_data);
 
 % orig
-cfg.sourcemodel.pos     = sourcemodel_orig.pos;
-cfg.sourcemodel.inside  = sourcemodel_orig.inside;
-cfg.sourcemodel.dim     = sourcemodel_orig.dim;
+% cfg.sourcemodel.pos     = sourcemodel_orig.pos;
+% cfg.sourcemodel.inside  = sourcemodel_orig.inside;
+% cfg.sourcemodel.dim     = sourcemodel_orig.dim;
 cfg.headmodel           = headmodel_orig; %% supply the headmodel
 dip_mag_late_orig = ft_dipolefitting(cfg, meg_data);
 
 % spm12
-cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
-cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
-cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
+% cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
+% cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
+% cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
 cfg.headmodel           = headmodel_spm12; %% supply the headmodel
-dip_mag_late_som12 = ft_dipolefitting(cfg, meg_data);
+dip_mag_late_spm12 = ft_dipolefitting(cfg, meg_data);
 
 
 %% Do dipole fit: gradiometer fits
 cfg = [];
-cfg.gridsearch          = 'yes'; %% search the grid for an optimal starting point
-cfg.dipfit.metric       = 'rv'; %% the metric to minimize (the relative residual variance: proportion of variance left unexplained by the dipole model)
-cfg.model               = 'regional'; %% we assume that the dipole has a fixed position during the time points in the latency range
-cfg.senstype            = 'MEG'; %% sensor type
-cfg.channel             = 'G'; %% which channels to use
-cfg.nonlinear           = 'yes'; %% do a non-linear search
+cfg.gridsearch          = 'yes';        %% search the grid for an optimal starting point
+cfg.dipfit.metric       = 'rv';         %% the metric to minimize (the relative residual variance: proportion of variance left unexplained by the dipole model)
+cfg.model               = 'regional';   %% we assume that the dipole has a fixed position during the time points in the latency range
+cfg.senstype            = 'MEG';        %% sensor type
+cfg.channel             = 'meggrad';     %% which channels to use
+cfg.grad                = grad;
+cfg.nonlinear           = 'yes';        %% do a non-linear search
 
 % FIRST COMPONENT
 cfg.latency             = early_latency; %% specify the latency
@@ -130,23 +132,23 @@ cfg.numdipoles          = 1; %% we only expect contralateral activity
 cfg.symmetry            = []; %% empty for single dipole fit
 
 % linear
-cfg.sourcemodel.pos     = sourcemodel_lin.pos;
-cfg.sourcemodel.inside  = sourcemodel_lin.inside;
-cfg.sourcemodel.dim     = sourcemodel_lin.dim;
+% cfg.sourcemodel.pos     = sourcemodel_lin.pos;
+% cfg.sourcemodel.inside  = sourcemodel_lin.inside;
+% cfg.sourcemodel.dim     = sourcemodel_lin.dim;
 cfg.headmodel           = headmodel_lin; %% supply the headmodel
 dip_grad_early_lin = ft_dipolefitting(cfg, meg_data);
 
 % orig
-cfg.sourcemodel.pos     = sourcemodel_orig.pos;
-cfg.sourcemodel.inside  = sourcemodel_orig.inside;
-cfg.sourcemodel.dim     = sourcemodel_orig.dim;
+% cfg.sourcemodel.pos     = sourcemodel_orig.pos;
+% cfg.sourcemodel.inside  = sourcemodel_orig.inside;
+% cfg.sourcemodel.dim     = sourcemodel_orig.dim;
 cfg.headmodel           = headmodel_orig; %% supply the headmodel
 dip_grad_early_orig = ft_dipolefitting(cfg, meg_data);
 
 % spm12
-cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
-cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
-cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
+% cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
+% cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
+% cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
 cfg.headmodel           = headmodel_spm12; %% supply the headmodel
 dip_grad_early_spm12 = ft_dipolefitting(cfg, meg_data);
 
@@ -156,23 +158,23 @@ cfg.numdipoles          = 2;    %% we expect bilateral activity
 cfg.symmetry            = 'x';
 
 % linear
-cfg.sourcemodel.pos     = sourcemodel_lin.pos;
-cfg.sourcemodel.inside  = sourcemodel_lin.inside;
-cfg.sourcemodel.dim     = sourcemodel_lin.dim;
+% cfg.sourcemodel.pos     = sourcemodel_lin.pos;
+% cfg.sourcemodel.inside  = sourcemodel_lin.inside;
+% cfg.sourcemodel.dim     = sourcemodel_lin.dim;
 cfg.headmodel           = headmodel_lin;
 dip_grad_late_lin = ft_dipolefitting(cfg, meg_data);
 
 % orig
-cfg.sourcemodel.pos     = sourcemodel_lin.pos;
-cfg.sourcemodel.inside  = sourcemodel_lin.inside;
-cfg.sourcemodel.dim     = sourcemodel_lin.dim;
+% cfg.sourcemodel.pos     = sourcemodel_lin.pos;
+% cfg.sourcemodel.inside  = sourcemodel_lin.inside;
+% cfg.sourcemodel.dim     = sourcemodel_lin.dim;
 cfg.headmodel           = headmodel_orig; %% supply the headmodel
 dip_grad_late_orig = ft_dipolefitting(cfg, meg_data);
 
 % spm12
-cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
-cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
-cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
+% cfg.sourcemodel.pos     = sourcemodel_spm12.pos;
+% cfg.sourcemodel.inside  = sourcemodel_spm12.inside;
+% cfg.sourcemodel.dim     = sourcemodel_spm12.dim;
 cfg.headmodel           = headmodel_spm12; %% supply the headmodel
 dip_grad_late_spm12 = ft_dipolefitting(cfg, meg_data);
 
@@ -181,12 +183,5 @@ save(fullfile(data_path, 'dip_mag_early.mat'), 'dip_mag_early*')
 save(fullfile(data_path, 'dip_mag_late.mat'), 'dip_mag_late*')
 save(fullfile(data_path, 'dip_grad_early.mat'), 'dip_grad_early*')
 save(fullfile(data_path, 'dip_grad_late.mat'), 'dip_grad_late*')
-
-%% Checks
-find(ismember(sourcemodel_spm12.pos, dip_mag_early_spm12.dip.pos))
-
-
-
-
 
 %END
